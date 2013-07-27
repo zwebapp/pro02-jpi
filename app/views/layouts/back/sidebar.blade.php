32e8e7a4959b0061bd6aec5a120d0fea540c6ba8
Z Webapp <zwebapp@gmail.com>
2013-07-27 14:04:05 +0800

    added ajax for categories

---
+++ app/views/layouts/back/sidebar.blade.php
@@ -0,0 +1,9 @@
+<div class="sidebar-nav">
+    <a href="{{ action('Dashboard@get_index') }}" class="btn btn-large btn-block">Dashboard</a>
+    <a href="#" class="btn btn-large btn-block ">Orders</a>
+    <a href="#" class="btn btn-large btn-block">Product</a>
+    <a href="{{ action('Categories@get_index') }}" class="btn btn-large btn-block">Categories</a>
+    <a href="#" class="btn btn-large btn-block">Clients</a>
+    <a href="#" class="btn btn-large btn-block">Agents</a>
+    <a href="#" class="btn btn-large btn-block">Administrators</a>
+</div><!--/.well -->
---
+++ app/views/layouts/back/top.blade.php
@@ -0,0 +1,7 @@
+<div class="span3">
+  <img src="{{ asset('public/img/logo.png') }}" alt="" />
+</div>
+<div class="span3">
+	
+</div>
+<div class="span6"></div>
