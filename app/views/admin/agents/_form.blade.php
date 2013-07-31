8169cac73a5397a93a1234945a18f1dc895eb48e
Z Webapp <zwebapp@gmail.com>
2013-07-31 22:53:16 +0800

    implemented agents page maintenance

---
+++ app/views/admin/agents/_form.blade.php
@@ -0,0 +1,59 @@
+<?php  if (isset($edit)) $info = json_decode($information); ?>
+
+{{ Form::open(array('action' => isset($edit) ? 'Agents@update' : 'Agents@save', 'method' => 'get', 'class' => 'ajax form-horizontal')) }}
+
+<div class="wrapper">
+  <h4> {{ isset($edit) ? "Edit " : "Add new " }} agent </h4>
+  
+  {{ Notification::showSuccess('<div class="alert alert-success"> :message </div>') }}
+  @if (isset($edit))
+  <div class="control-group">
+    <label class="control-label" for="is_active">Active: </label>
+    <div class="controls">
+      {{ Form::checkbox('is_active', '', $is_active , ['class' => 'for-switch']) }}
+    </div>
+  </div>
+  @endif
+  <div class="control-group">
+    <label class="control-label" for="full_name">Fullname: </label>
+    <div class="controls">
+      {{ Form::text('full_name', isset($info->full_name) ? $info->full_name : '', array('class' => 'span3', 'placeholder' => 'e.g: Juan Dela Cruz')) }}
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="email_address">Email: </label>
+    <div class="controls">
+      {{ Form::text('email_address', isset($info->email_address) ? $info->email_address : '', array('class' => 'span3', 'placeholder' => 'e.g: juan@delacruz.com')) }}
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="address">Address:</label>
+    <div class="controls">
+      {{ Form::textarea('address', isset($info->address) ?$info->address : '', array('class' => 'span3', 'row' => '3', 'placeholder' => 'e.g: Somewhere down the road')) }}
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="birthday">Birthday:</label>
+    <div class="controls">
+      {{ Form::text('birthday', isset($info->birthday) ? $info->birthday : '', array( 'data-mask' => '99-99-9999', 'class' => 'span2', 'placeholder' => 'e.g: 03-03-1983')) }}
+    </div>
+  </div>
+  <div class="control-group">
+    <label class="control-label" for="contact_no">Contact #:</label>
+    <div class="controls">
+      {{ Form::text('contact_no', isset($info->contact_no) ? $info->contact_no : '', array('class' => 'span2', 'placeholder' => 'e.g: 123-33-3312')) }}
+    </div>
+  </div>
+
+</div>
+
+<div class="modal-footer">
+  {{ HTML::image('public/img/preload.gif', '', ['class' => 'hidden', 'id' => 'preload']) }}
+  @if (isset($id))
+    {{ Form::hidden('id', $id) }}
+  @endif
+  {{ Form::button('Close', array('data-dismiss' => 'modal', 'aria-hidden' => 'true', 'class' => 'btn')) }}
+  {{ Form::submit(isset($edit) ? 'Update' : 'Add Agent', array('class' => 'btn btn-primary', 'data-refresh' => '.modal-body')) }}
+</div>
+
+{{ Form::close() }}
---
+++ app/views/admin/agents/main.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.back.master')
+
+@section('title')
+  Manage Agents | Administrators Page | JPI 
+@stop
+
+@section('content')
+
+  @include('_partials.errors')
+  
+  <div class="main-wrapper">
+    <div class="row-fluid">
+      <div class="span3">
+        <a href="{{ url('admin/agents/add') }}" class="btn btn-primary" data-toggle="modal" id="addNewAgent">Add new agent</a>
+      </div>
+      <div class="span3"></div>
+      <div class="span6">
+        @include('_partials.search')
+      </div>
+    </div>
+    <div class="row-fluid">
+      <div class="span12">
+        <table id="dataTable" class="table table-hover table-condensed table-striped">
+          <thead>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Birthday</th>
+            <th>Contact #</th>
+            <th>Actions</th>
+          </thead>
+          <tbody>
+          @foreach (Agent::all() as $agent)
+            <?php $info = json_decode($agent->information); ?>
+            <tr class="item_{{ $agent->id }}">
+              <td class="name">{{ $info->full_name }}</td>
+              <td class="contact_no">{{ $info->email_address }}</td>
+              <td class="address">{{ $info->address }}</td>
+              <td class="birthday">{{ $info->birthday }}</td>
+              <td class="contact_no">{{ $info->contact_no }}</td>
+              <td>
+                <a class="btn btn-mini edit" href="{{ url('admin/agents/' . $agent->id) . '/edit' }}" data-toggle="modal" title="Edit"><i class="icon-pencil"></i> </a>
+                <a class="btn btn-mini delete" href="{{ url('admin/agents/' . $agent->id . '/remove')  }}" ><i class="icon-trash"></i> </a>
+              </td>
+            </tr>
+          @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+@stop
+
+@section('extraHtml')
+
+<!-- Modal -->
+<div id="modalBox" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> </button>
+  
+  <div class="modal-body">
+    <div class="ajax-wrapper">
+      {{ HTML::image('public/img/ajax-loader.gif'); }}
+    </div>
+  </div>
+
+</div>
+
+@stop
